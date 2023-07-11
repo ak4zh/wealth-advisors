b6e4d116265257cf076d89f3d08c6c9bdf6f9b0e
ak4zh <agwl.akash@gmail.com>
2023-07-11 14:37:39 +0530

    Update Contact.svelte

--- src/lib/Contact.svelte
+++ src/lib/Contact.svelte
@@ -5,5 +5,6 @@
     <br>
     Raipur, Chhattisgarh 492001, India
     <br>
+    <strong>Email:</strong> <a href="mailto:team@getwealthadvisors.com">team@getwealthadvisors.com</a>
     <strong>Phone:</strong> +91-8109136362
 </div>
